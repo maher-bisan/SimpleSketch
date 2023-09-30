bbe774798067393cafd26d3170917530d23a22de
Maher Bisan <maherbisan95@gmail.com>
2023-09-30 23:38:57 +0300

    updated cli

--- src/simple_sketch/simple_sketch_cli/simple_sketch_cli.py
+++ src/simple_sketch/simple_sketch_cli/simple_sketch_cli.py
@@ -3,3 +3,13 @@
 
 def simple_sketch_cli():
     print("HELLO FROM ---> simple_sketch_cli")
+    print("""
+The CLI is not yet implemented.
+In the meantime, you can use the GUI by running:
+'simple_sketch_gui'
+Or by running:
+'python -m simple_sketch.simple_sketch_gui.simple_sketch_gui'
+""")
+
+if __name__ == "__main__":
+    simple_sketch_cli()
